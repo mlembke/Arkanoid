65689615bc7f9cef450fc2268bd9f4ddc076656b
Marcin Lembke <marcin.lembke@gmail.com>
2015-12-03 18:38:57 +0100

    Added Texture2D class for binding textures

---
+++ Texture2D.cpp
@@ -0,0 +1,33 @@
+#include <windows.h>
+#include "Texture2D.h"
+
+
+
+Texture2D::Texture2D() : width_(0), height_(0), internalFormat_(GL_RGB), imageFormat_(GL_RGB), wrapSAxis_(GL_REPEAT), wrapTAxis_(GL_REPEAT), filterMin_(GL_LINEAR), filterMax_(GL_LINEAR)
+{
+	OutputDebugStringW(L"Texture2D::Texture2D()\n");
+	glGenTextures(1, &id_);
+}
+
+
+void Texture2D::generate(GLuint width, GLuint height, unsigned char* data)
+{
+	OutputDebugStringW(L"Texture2D::generate()\n");
+	width_ = width;
+	height_ = height;
+
+	glBindTexture(GL_TEXTURE_2D, id_);
+	glTexImage2D(GL_TEXTURE_2D, 0, internalFormat_, width, height, 0, imageFormat_, GL_UNSIGNED_BYTE, data);
+
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, wrapSAxis_);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, wrapTAxis_);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, filterMin_);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, filterMax_);
+
+	glBindTexture(GL_TEXTURE_2D, 0);
+}
+
+void Texture2D::Bind() const
+{
+	glBindTexture(GL_TEXTURE_2D, id_);
+}
---
+++ Texture2D.h
@@ -0,0 +1,25 @@
+#pragma once
+#define GLEW_STATIC
+#include <GL/glew.h>
+
+
+class Texture2D
+{
+public:
+	Texture2D();
+	~Texture2D() = default;
+
+	void generate(GLuint width, GLuint height, unsigned char* data);
+	void Bind() const;
+
+	GLuint id_;
+	GLuint width_;
+	GLuint height_;
+	GLuint internalFormat_;
+	GLuint imageFormat_;
+	GLuint wrapSAxis_;
+	GLuint wrapTAxis_;
+	GLuint filterMin_;
+	GLuint filterMax_;
+};
+
