4be51897dfb150ff7bd96b60ce85555864742f4a
Marcin Lembke <marcin.lembke@gmail.com>
2015-12-03 20:47:56 +0100

    Added SpriteRenderer class used for drawing sprites

---
+++ SpriteRenderer.cpp
@@ -0,0 +1,70 @@
+#include <windows.h>
+#include "SpriteRenderer.h"
+#include <glm/gtc/matrix_transform.hpp>
+
+SpriteRenderer::SpriteRenderer(const Shader& shader) : shader_(shader)
+{
+	OutputDebugStringW(L"SpiriteRenderer::SpiriteRenderer() -> default constructor\n");
+	init();
+}
+
+
+SpriteRenderer::~SpriteRenderer()
+{
+	glDeleteVertexArrays(1, &quadVAO_);
+}
+
+void SpriteRenderer::drawSprite(Texture2D& texture, glm::vec2 position, glm::vec2 size, GLfloat rotate, glm::vec3 color)
+{
+	OutputDebugStringW(L"SpiriteRenderer::drawSpirite()\n");
+	shader_.use();
+	glm::mat4 model;
+	model = glm::translate(model, glm::vec3(position, 0.0f));  // First translate (transformations are: scale happens first, then rotation and then finall translation happens; reversed order)
+
+	model = glm::translate(model, glm::vec3(0.5f * size.x, 0.5f * size.y, 0.0f)); // Move origin of rotation to center of quad
+	model = glm::rotate(model, rotate, glm::vec3(0.0f, 0.0f, 1.0f)); // Then rotate
+	model = glm::translate(model, glm::vec3(-0.5f * size.x, -0.5f * size.y, 0.0f)); // Move origin back
+
+	model = glm::scale(model, glm::vec3(size, 1.0f)); // Last scale
+
+	shader_.setMatrix4("model", model);
+
+	// Render textured quad
+	shader_.setVector3f("spriteColor", color);
+
+	glActiveTexture(GL_TEXTURE0);
+	texture.Bind();
+
+	glBindVertexArray(quadVAO_);
+	glDrawArrays(GL_TRIANGLES, 0, 6);
+	glBindVertexArray(0);
+}
+
+void SpriteRenderer::init()
+{
+	OutputDebugStringW(L"SpiriteRenderer::init()\n");
+	// Configure VAO/VBO
+	GLuint VBO;
+	GLfloat vertices[] = {
+		// Pos      // Tex
+		0.0f, 1.0f, 0.0f, 1.0f,
+		1.0f, 0.0f, 1.0f, 0.0f,
+		0.0f, 0.0f, 0.0f, 0.0f,
+
+		0.0f, 1.0f, 0.0f, 1.0f,
+		1.0f, 1.0f, 1.0f, 1.0f,
+		1.0f, 0.0f, 1.0f, 0.0f
+	};
+
+	glGenVertexArrays(1, &quadVAO_);
+	glGenBuffers(1, &VBO);
+
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+	glBindVertexArray(quadVAO_);
+	glEnableVertexAttribArray(0);
+	glVertexAttribPointer(0, 4, GL_FLOAT, GL_FALSE, 4 * sizeof(GLfloat), static_cast<GLvoid*>(nullptr));
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
+	glBindVertexArray(0);
+}
---
+++ SpriteRenderer.h
@@ -0,0 +1,32 @@
+#pragma once
+#define GLEW_STATIC
+#include <GL/glew.h>
+#include <glm/glm.hpp>
+#include "Texture2D.h"
+#include "Shader.h"
+
+/**
+* \file ResourceManger.h
+* \brief Header file of SpriteRenderer class.
+*/
+/**
+* SpriteRenderer
+* Class defines Sprite Renderer used for drawing sprites.
+*/
+class SpriteRenderer
+{
+public:
+	SpriteRenderer() = default;
+	explicit SpriteRenderer(const Shader& shader);
+
+	~SpriteRenderer();
+
+	void drawSprite(Texture2D& texture, glm::vec2 position, glm::vec2 size = glm::vec2(10, 10), GLfloat rotate = 0.0f, glm::vec3 color = glm::vec3(1.0f));
+
+private:
+	void init();
+	Shader shader_;
+	GLuint quadVAO_;
+	
+};
+
